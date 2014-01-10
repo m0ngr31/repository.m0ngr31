5a78c888a5037209d3dba939d85e56f92300b81c
m0ngr31 <joeipson@gmail.com>
2014-01-10 13:49:15 -0700

    Try, try, again

--- updateRepo.sh
+++ updateRepo.sh
@@ -1,6 +1,6 @@
-cp -rf ../plugin.video.espn3 download/.
-git rm -f --cached download/plugin.video.espn3
+cp -rf ../plugin.video.espn3 download/
+git rm -f --cached ./download/plugin.video.espn3
 git add -A
-git add plugin.video.espn3/*
+git add ./download/plugin.video.espn3/*
 git commit
 git push origin master
