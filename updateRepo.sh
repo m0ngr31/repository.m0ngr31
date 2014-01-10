caab54981565edfb92aed0b439444f813af9e7d4
m0ngr31 <joeipson@gmail.com>
2014-01-10 13:59:25 -0700

    myaaha!

--- updateRepo.sh
+++ updateRepo.sh
@@ -1,5 +1,6 @@
 rm -rf ./download/plugin.video.espn3
 cp -rf ../plugin.video.espn3 ./download/
+rm -rf ./download/plugin.video.espn3/.git
 git rm -rf --cached ./download
 git add -A
 git add ./download/plugin.video.espn3/*
